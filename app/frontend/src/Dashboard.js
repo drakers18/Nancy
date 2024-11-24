54602b5fe6a5fc5ba30bd5a0c4f301eaaa1c3567
Matt-Drong <mathias.drong@creospan.com>
2024-11-24 12:30:24 -0600

    Update Dashboard.js

--- app/frontend/src/Dashboard.js
+++ app/frontend/src/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import InvestModal from "./components/InvestModal";
+import DialogFlowChatbot from "./components/DialogFlow";
 // core components
 import "./App.css"
 //import DB from '../material-dashboard-master/material-dashboard-master/pages/dashboard.html'
@@ -13,13 +14,16 @@ const Dashboard = () =>{
      
     return (
        <>
-
+      
 
 
        <p>Welcome to the Dashboard</p>
        <InvestModal></InvestModal>
      
 
+        <DialogFlowChatbot/>
+      
+  
      </>
     )
 }
